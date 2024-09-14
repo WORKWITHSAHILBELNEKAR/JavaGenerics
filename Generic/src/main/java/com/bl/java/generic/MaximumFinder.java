89d5dfa0cfd71c1ff3e6e9284847f3db69dd2fe2
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-09-14 10:05:35 +0530

    all task added

---
+++ Generic/src/main/java/com/bl/java/generic/MaximumFinder.java
@@ -0,0 +1,38 @@
+package com.bl.java.generic;
+
+public class MaximumFinder {
+
+    // Generic method to find the maximum of three comparable objects
+
+    public static <T extends Comparable<T>> T findMaximum(T x, T y, T z)
+    {
+        T max = x;  // Assume x is the maximum
+
+        if (y.compareTo(max) > 0) {
+            max = y;  // y is the new maximum
+        }
+
+        if (z.compareTo(max) > 0) {
+            max = z;  // z is the new maximum
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Find Maximum Problem using Generics");
+
+        // Test Case
+        Integer maxInt = findMaximum(14, 24, 11);
+        float maxFloat = findMaximum(6.5f, 8.4f, 12.3f);
+        String maxString = findMaximum("Apple", "Peach", "Banana");
+
+        // Print Result
+        System.out.println("Maximum Number is: " + maxInt); // Expected: 10
+        System.out.println("Maximum Number is: " + maxFloat); // Expected 12.3
+        System.out.println("Maximum String is: " + maxString); // Expected Peach
+
+    }
+
+}
